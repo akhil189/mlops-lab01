b368d06530c1ba06c9dacbbc92ae823c0ddcd013
Akhil <saiakhilesh.a18@gmail.com>
2023-09-24 10:31:10 -0400

    updated test and gitignore files

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -1,10 +1,12 @@
-import pytest
-import os, sys
+import sys
+import os
+import unittest
 
+# Get the path to the project's root directory
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(project_root)
 
-import src.calculator as calulator
+from src import calculator
 
 def test_fun1():
     assert calculator.fun1(2, 3) == 5
